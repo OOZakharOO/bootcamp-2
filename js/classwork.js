99c1667aaf06ebd6f76b80ea8bf26e34e7335ec1
OOZakharOO <stolyarzahar1@gmail.com>
2020-06-25 22:44:08 +0300

    homeworkJS is done

---
+++ js/classwork.js
@@ -0,0 +1,57 @@
+// переменные let и conts
+let name; //объявление переменной
+console.log(name); 
+
+name = "Aleksa";
+console.log(name);
+
+name = "Aleksandra"; //переприсвоение
+console.log(name);
+
+const lastName = "Hrevtsova";
+console.log(lastName);
+
+// lastName = "fasfrgsf" - нельзя переназначить
+
+
+// типы данных
+
+// примитивы
+
+// 1- String ('', "", '')
+let myName = "Aleksandra";
+console.log(myName); // Aleksandra
+console.log(typeof myName) // string
+
+let count = "30";
+console.log(count);
+console.log(typeof count);
+// 2- Number
+let age = 30;
+console.log(age);
+console.log(typeof age); // number
+
+// 3- Boolean
+let isOnline = true;
+console.log(isOnline);
+console.log(typeof isOnline)
+
+isOnline = false;
+console.log(isOnline);
+console.log(typeof isOnline);
+
+// 4- Underfined
+let products;
+console.log(products);
+console.log(typeof isOnline);
+
+// 5- Null
+let cart = null;
+console.log(cart);
+console.log(typeof cart);
+// сложные
+
+// Object
+// Array
+// Function
+// Symbol
---
+++ js/homework.js
@@ -0,0 +1,10 @@
+let name = "Zakhar";
+console.log(name);
+console.log(typeof name);
+
+const lastName = "Stolyarchuk";
+console.log(lastName);
+
+let age = 13;
+console.log(age);
+console.log(typeof age)
